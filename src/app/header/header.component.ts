277a3e7089abe79b937f9d777005c9319d9e558a
Emile Cyimena <cyimena09@hotmail.com>
2021-06-27 15:55:06 +0200

    add check if mobile function

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -38,7 +38,19 @@ export class HeaderComponent implements OnInit {
   }
 
   onLoadCv() {
-    window.open('/assets/cv/emile_cyimena_cv_pdf.pdf', '_blank');
+    if (this.isMobileDevice()) {
+      window.open('/assets/cv/emile_cyimena_cv_pdf.pdf', '_blank');
+    }
+  }
+
+  isMobileDevice() {
+    return !!(navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i));
   }
 
 }
